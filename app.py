e7d28b309e756be18fce8e503941eb2e390fd61d
Bilal Hassan <bilal6322800@gmail.com>
2025-10-03 03:36:42 +0200

    Sahi commit message

---
+++ app.py
@@ -0,0 +1,3 @@
+# print ("Ye command feature/login branch ko main branch mein merge karta hai.")
+# print ("--no-ff ka matlab hai ki Git ek separate merge commit banayega,")
+# print ("jisse ye clear rahega ki ek alag feature branch merge hui thi. Ye future mein tracking ke liye helpful hota hai.")
